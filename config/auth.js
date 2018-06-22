0f8d224822a178d0a05340ecd01be3647a330817
JerryPeng0201 <jerrypeng666@brandeis.edu>
2018-06-22 13:32:10 -0400

    auth update

--- config/auth.js
+++ config/auth.js
@@ -4,7 +4,7 @@ module.exports = {
     'googleAuth' : {
         'clientID': '499451827902-hukpdqgjno73eav7vmvbo0s3qfl3ckkh.apps.googleusercontent.com',
         'clientSecret'  : 'He9zBik7BSrkfe8AEMM_g3TG',
-        'callbackURL'   : 'http://129.64.154.14/login/authorized'
+        'callbackURL'   : 'http://leiner.cs-i.brandeis.edu:5100/login/authorized'
     }
 
 };
